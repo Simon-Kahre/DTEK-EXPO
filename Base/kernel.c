9534c63d5f9b8d862391727f7141c78de860ee9e
Leo Ehrenberg <leoeh@kth.se>
2025-12-02 02:24:09 +0100

    changed the border handling to match whats in the wikipedia page, also making the code run faster

--- Base/kernel.c
+++ Base/kernel.c
@@ -14,8 +14,8 @@ enum {
     UNSHARP_MASKING
 };
 
-static void applyKernel(int w, int h, volatile Pixel src[w][h], volatile Pixel dst[w][h], const double *k, int radius);
-void imageProcessing(int w, int h, volatile Pixel src[w][h], volatile Pixel dst[w][h], int option);
+static void applyKernel(int w, int h, volatile Pixel input[w][h], volatile Pixel output[w][h], const double *k, int radius);
+void imageProcessing(int w, int h, volatile Pixel input[w][h], volatile Pixel output[w][h], int option);
 
 const double ridgeDetection[3][3] =
 {
@@ -70,33 +70,33 @@ const double unsharpMasking[5][5] =
     {-1.0/256.0, -4.0/256.0, -6.0/256.0, -4.0/256.0, -1.0/256.0},
 };
 
-void imageProcessing(int w, int h, volatile Pixel src[w][h], volatile Pixel dst[w][h], int option){
+void imageProcessing(int w, int h, volatile Pixel input[w][h], volatile Pixel output[w][h], int option){
     switch (option) {
         case RIDGE_DETECTION:
-            applyKernel(w, h, src, dst, &ridgeDetection[0][0], 1);
+            applyKernel(w, h, input, output, &ridgeDetection[0][0], 1);
             break;
         case EDGE_DETECTION:
-            applyKernel(w, h, src, dst, &edgeDetection[0][0], 1);
+            applyKernel(w, h, input, output, &edgeDetection[0][0], 1);
             break;
         case SHARPEN:
-            applyKernel(w, h, src, dst, &sharpen[0][0], 1);
+            applyKernel(w, h, input, output, &sharpen[0][0], 1);
             break;
         case BOX_BLUR:
-            applyKernel(w, h, src, dst, &boxBlur[0][0], 1);
+            applyKernel(w, h, input, output, &boxBlur[0][0], 1);
             break;
         case GAUSSIAN_BLUR3X3:
-            applyKernel(w, h, src, dst, &gaussianBlur3x3[0][0], 1);
+            applyKernel(w, h, input, output, &gaussianBlur3x3[0][0], 1);
             break;
         case GAUSSIAN_BLUR5X5:
-            applyKernel(w, h, src, dst, &gaussianBlur5x5[0][0], 2);
+            applyKernel(w, h, input, output, &gaussianBlur5x5[0][0], 2);
             break;
         case UNSHARP_MASKING:
-            applyKernel(w, h, src, dst, &unsharpMasking[0][0], 2);
+            applyKernel(w, h, input, output, &unsharpMasking[0][0], 2);
             break;
         default:
             for (int x = 0; x < w; ++x){
                 for(int y = 0; y < h; ++y){
-                    dst[x][y] = src[x][y];
+                    output[x][y] = input[x][y];
                 }
             } 
             break;
@@ -105,15 +105,21 @@ void imageProcessing(int w, int h, volatile Pixel src[w][h], volatile Pixel dst[
 
 static unsigned char clamp255(double v){
     if (v < 0.0) v = 0.0;
-    if( v > 255.0) v = 255.0;
+    if (v > 255.0) v = 255.0;
     return (unsigned char) v;
 }
 
-static void applyKernel(int w, int h, volatile Pixel src[w][h], volatile Pixel dst[w][h], const double *k, int radius){
+static int clamp_int(int v, int max){
+    if (v < 0) return 0;
+    if (v > max) return max;
+    return v;
+}
+
+static void applyKernel(int w, int h, volatile Pixel input[w][h], volatile Pixel output[w][h], const double *k, int radius){
     int size = 2 * radius + 1;
 
-    for (int x = radius; x < w -radius; ++x){
-        for (int y = radius; y < h-radius; ++y){
+    for (int x = 0; x < w; ++x){
+        for (int y = 0; y < h; ++y){
             double R = 0.0, G = 0.0, B = 0.0;
 
             for (int dx = -radius; dx <= radius; ++dx){
@@ -122,28 +128,19 @@ static void applyKernel(int w, int h, volatile Pixel src[w][h], volatile Pixel d
                     int ky = dy + radius;
                     double c = k[kx * size + ky];
 
-                    Pixel p = src[x + dx][y + dy];
+                    int sx = clamp_int(x+dx, w-1);
+                    int sy = clamp_int(y+dy, h-1);
+
+                    Pixel p = input[sx][sy];
                     R += c * (unsigned char)p.r;
                     G += c * (unsigned char)p.g;
                     B += c * (unsigned char)p.b;
                 }
             }
 
-            dst[x][y].r = clamp255(R);
-            dst[x][y].g = clamp255(G);
-            dst[x][y].b = clamp255(B);
-        }
-    }
-    for (int x = 0; x<w; ++x){
-        for (int y = 0; y < radius; ++y){
-            dst[x][y] = src[x][y];
-            dst[x][h-1-y] = src[x][h-1-y];
-        }
-    }
-    for (int y = 0; y<h; ++y){
-        for(int x = 0; x < radius; ++x){
-            dst[x][y] = src[x][y];
-            dst[w-1-x][y] = src[w-1-x][y];
+            output[x][y].r = clamp255(R);
+            output[x][y].g = clamp255(G);
+            output[x][y].b = clamp255(B);
         }
     }
 }
